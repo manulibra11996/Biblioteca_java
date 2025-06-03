168ad6f5a27355a7dccaf2ec8ee3edf4721c3eba
CurriFranciscoDE <Curry_gsx6@hotmail.com>
2025-06-03 19:24:51 +0200

    partes bibloteca autor

---
+++ src/main/java/com/adecco/biblioteca/Proyecto_final/EntidadAutor.java
@@ -0,0 +1,47 @@
+package com.adecco.biblioteca.Proyecto_final;
+
+public class EntidadAutor {package com.Autor.proyecto.entidad;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+    @Entity
+    public class Autor {
+
+        @Id
+        @GeneratedValue(strategy = GenerationType.IDENTITY)
+        private Long id;
+
+        private String nombre;
+        private String apellido;
+
+        // Getters y setters
+        public Long getId() {
+            return id;
+        }
+
+        public void setId(Long id) {
+            this.id = id;
+        }
+
+        public String getNombre() {
+            return nombre;
+        }
+
+        public void setNombre(String nombre) {
+            this.nombre = nombre;
+        }
+
+        public String getApellido() {
+            return apellido;
+        }
+
+        public void setApellido(String apellido) {
+            this.apellido = apellido;
+        }
+    }
+
+
+}
---
+++ src/main/java/com/adecco/biblioteca/Proyecto_final/controllerAutor.java
@@ -0,0 +1,38 @@
+package com.adecco.biblioteca.Proyecto_final;
+
+public class controllerAutor {// src/main/java/com/ejemplo/proyecto/controlador/AutorController.java
+
+package com.autor.proyecto.controlador;
+
+import com.autor.proyecto.entidad.Autor;
+import com.autor.proyecto.servicio.AutorService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+    @RestController
+    @RequestMapping("/api/autores")
+    public class AutorController {
+
+        @Autowired
+        private AutorService autorService;
+
+        @GetMapping
+        public ResponseEntity<List<Autor>> getAllAutores() {
+            return new ResponseEntity<>(autorService.getAllAutores(), HttpStatus.OK);
+        }
+
+        @GetMapping("/{id}")
+        public ResponseEntity<Autor> getAutorById(@PathVariable Long id) {
+            return new ResponseEntity<>(autorService.getAutorById(id), HttpStatus.OK);
+        }
+
+        @PostMapping
+        public ResponseEntity<Autor> saveAutor(@RequestBody Autor autor) {
+            return new ResponseEntity<>(autorService.saveAutor(autor), HttpStatus.CREATED);
+  }
+
+    }
