5aba37691a2f1e28b29f7844d21d025713675c6a
Manuel Fernández Fernández <86914767+manulibra11996@users.noreply.github.com>
2025-06-06 15:57:07 +0200

    Subiendo prestamos

---
+++ src/main/java/com/adecco/biblioteca/Proyecto_final/entidades/Prestamo.java
@@ -0,0 +1,93 @@
+package com.adecco.biblioteca.Proyecto_final.entidades;
+
+import jakarta.persistence.*;
+
+import java.util.Date;
+
+    @Entity // indica que esta clase es una tabla
+    @Table(name = "prestamo") // personaliza el nombre de la tabla
+    public class Prestamo {
+
+        @Id // indica que este atributo es la clave primaria
+        @GeneratedValue(strategy = GenerationType.IDENTITY)
+        private Long id;
+
+        //MUCHOS alquileres pertenece a UN socio
+        //@ManyToOne
+        //@JoinColumn(name = "socio_id")
+        private String socio;//Modificar por el tipo Socio
+
+        //MUCHOS alquileres pertenece a UN libro
+        //@ManyToOne
+        //@JoinColumn(name = "libro_id")
+        private String libro;//Modificar por el tipo Libro
+
+        private Date fechaEntrega;
+
+        private Date fechaDevolucion;
+
+        // constructor vacío
+        public Alquiler() {
+        }
+
+        // constructor con parámetros
+        public Alquiler(String socio, String libro, Date fechaEntrega, Date fechaDevolucion) {
+            this.socio = socio;
+            this.libro = libro;
+            this.fechaEntrega = fechaEntrega;
+            this.fechaDevolucion = fechaDevolucion;
+        }
+
+        // getters y setters
+        public Long getId() {
+            return id;
+        }
+
+        public void setId(Long id) {
+            this.id = id;
+        }
+
+        public String getSocio() {
+            return socio;
+        }
+
+        public void setSocio(String socio) {
+            this.socio = socio;
+        }
+
+        public String getLibro() {
+            return libro;
+        }
+
+        public void setLibro(String libro) {
+            this.libro = libro;
+        }
+
+        public Date getFechaEntrega() {
+            return fechaEntrega;
+        }
+
+        public void setFechaEntrega(Date fechaEntrega) {
+            this.fechaEntrega = fechaEntrega;
+        }
+
+        public Date getFechaDevolucion() {
+            return fechaDevolucion;
+        }
+
+        public void setFechaDevolucion(Date fechaDevolucion) {
+            this.fechaDevolucion = fechaDevolucion;
+        }
+
+        @Override
+        public String toString() {
+            return "Alquiler{" +
+                    "id=" + id +
+                    ", socio='" + socio + '\'' +
+                    ", libro='" + libro + '\'' +
+                    ", fechaEntrega=" + fechaEntrega +
+                    ", fechaDevolucion=" + fechaDevolucion +
+                    '}';
+        }
+    }
+
---
+++ src/main/java/com/adecco/biblioteca/Proyecto_final/repositorios/prestamoRepositorio.java
@@ -0,0 +1,24 @@
+package com.adecco.biblioteca.Proyecto_final.repositorios;
+
+import com.adecco.biblioteca.Proyecto_final.entidades.Prestamo;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+    public interface PrestamoRepositories extends JpaRepository<Prestamo, Long> {
+
+        // spring boot implementa métodos básicos:
+        // findAll(), findById(), save(), delete(), etc...
+
+        // métodos derivados
+        // Busqueda por usuario
+
+        //Busqueda por libro
+
+        //Busqueda de libros no devueltos
+
+        //Busqueda de libros devueltos
+
+        //Libros sacados desde 2 fechas
+
+        //Libros devueltos desde 2 fechas
+    }
+
